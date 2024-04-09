d03ef4cafb6bf6524adb4e311569e59d049acca0
Yeoeun Yang <yyang78077@naver.com>
2024-04-09 15:25:34 +0900

    post commit

---
+++ src/entities/post.entity.ts
@@ -0,0 +1,27 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    JoinColumn
+} from 'typeorm';
+import { UserEntity } from './user.entity';
+
+@Entity()
+export class PostEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    content: string;
+
+    @Column()
+    likes: number;
+
+    //FK
+    @ManyToOne(() => UserEntity)
+    author_id: UserEntity;
+}
