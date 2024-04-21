b2cfd0dd2a6cbf517d3ebbdd264aad95000cddf1
Yeoeun Yang <yyang78077@naver.com>
2024-04-21 16:06:14 +0900

    crud commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,27 +1,62 @@
-import { Body, Controller, Get, Post, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Put, UseFilters } from '@nestjs/common';
 import { UserService } from './user.service';
+import { PostEntity, UserEntity } from 'src/entities';
+import { HttpExceptionFilter } from 'src/filter/http-exception.filter';
 
 @Controller('user')
+@UseFilters(HttpExceptionFilter) //컨트롤러별로 적용
 export class UserController {
     constructor(private readonly userService: UserService){}
 
-    @Get()
-    getUsers(): string[] {
+    @Get('/user')
+    async getUsers(): Promise<UserEntity[]> {
         return this.userService.getUsers();
     }
 
-    @Post()
-    async addUser(@Body() info): Promise<string[]> {
-        return this.userService.addUser(info.name);
+    @Post('/user')
+    async addUser(@Body() info) {
+        return this.userService.addUser(info);
     }
 
-    @Delete()
-    deleteUser(@Body() info): Promise<string[]> {
-        return this.userService.deleteUser(info.name);
+    @Delete('/user')
+    deleteUser(@Body() info) {
+        return this.userService.deleteUser(info.email);
     }
 
-    @Get('/:index')
-    getUserName(@Param('index') index: number): string {
-        return this.userService.getUserName(index);
+    @Get('/user/find')
+    async findByEmail(@Body() info) {
+        return this.userService.findByEmail(info.email);
     }
+
+    @Put('/user/password')
+    updatePassword(@Body() info) {
+        return this.userService.updatePassword(info.email, info.password);
+    }
+    
+    //이 아래가 Post CRUD
+    @Get('/post')
+    async getPosts(): Promise<PostEntity[]> {
+        return this.userService.getPosts();
+    }
+
+    @Post('/post')
+    async addPost(@Body() info) {
+        return this.userService.addPost(info.title, info.content, info.likes, info.author_id);
+    }
+
+    @Delete('/post')
+    deletePost(@Body() info) {
+        return this.userService.deletePost(info.id);
+    }
+
+    @Get('/post/find')
+    async findById(@Body() info) {
+        return this.userService.findById(info.id);
+    }
+
+    @Put('/post/content')
+    updateContent(@Body() info) {
+        return this.userService.updateContent(info.id, info.content);
+    }
+
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserEntity } from 'src/entities';
+import { PostEntity } from 'src/entities';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, PostEntity])], 
   controllers: [UserController],
   providers: [UserService]
 })
